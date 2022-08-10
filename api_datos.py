13f2d59cb6febd83039136fb00667bcc0720a889
Alexis Yafel Garcia <a.yafel10@gmail.com>
2022-08-09 21:04:37 -0500

    Se realizaron mejoras

--- api_datos.py
+++ api_datos.py
@@ -3,9 +3,10 @@ import pprint
 
 import requests
 
-url = 'https://api.datos.gob.mx/v1/calidadAire'
-lista = []
-if __name__ == '__main__':
+
+def api_response():
+    url = 'https://api.datos.gob.mx/v1/calidadAire'
+    lista = []
     data = requests.get(url)
     if data.status_code == 200:
         # pprint.pprint(data.json().get('results'))
@@ -19,3 +20,22 @@ if __name__ == '__main__':
                         value = j['value'], j['unit'], j['pollutant']
                         lista.append(value)
                         print(f'El valor de: 58c780bf281e87010078f491 es:', lista)
+                        return j['pollutant']
+
+
+def encontrar_valor(populant):
+    url = 'https://api.datos.gob.mx/v1/calidadAire'
+    lista = []
+    data = requests.get(url)
+    data = data.json().get('results')
+    ultimo = data[-1]['stations']
+    for e in ultimo:
+        mesau = e['measurements']
+        for j in mesau:
+            value = j['value'], j['unit'], j['pollutant']
+            new_valor = populant
+            print(value, new_valor)
+
+
+value_popullant = api_response()
+encontrar_valor(value_popullant)
