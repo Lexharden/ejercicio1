6004e95f7f8fd7791c1b33185a11561a7f063cde
Alexis Yafel Garcia <a.yafel10@gmail.com>
2022-08-09 21:09:22 -0500

    Se realizaron mejoras

--- api_datos.py
+++ api_datos.py
@@ -3,7 +3,7 @@ import pprint
 
 import requests
 
-
+# Funcion obtener resultados de api e imprimirlos en una lista
 def api_response():
     url = 'https://api.datos.gob.mx/v1/calidadAire'
     lista = []
@@ -22,7 +22,7 @@ def api_response():
                         print(f'El valor de: 58c780bf281e87010078f491 es:', lista)
                         return j['pollutant']
 
-
+# Funcion que encuentra el ultimo valor y agrega el mismo valor del ejercicio anterior
 def encontrar_valor(populant):
     url = 'https://api.datos.gob.mx/v1/calidadAire'
     lista = []
@@ -36,6 +36,6 @@ def encontrar_valor(populant):
             new_valor = populant
             print(value, new_valor)
 
-
+# Se ejecutan las funciones y se almacena su valor en una variable
 value_popullant = api_response()
 encontrar_valor(value_popullant)
